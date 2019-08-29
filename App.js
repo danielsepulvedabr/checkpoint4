6371f4530e960908ae2f9e9b9d372004aba2031f
danielsepulvedabr <52954463+danielsepulvedabr@users.noreply.github.com>
2019-08-29 11:39:01 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import "./syle.css" ;
+import Wrapper from './Wrapper'
+import Photo from './Photo'
+function App() {
+  return (
+    <div className="App"> 
+    
+     <Wrapper/>
+      
+  </div>
+
+    
+  );
+  }
+
+export default App;
